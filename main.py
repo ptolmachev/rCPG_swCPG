139507b2c733d7f21b9ef979315a15b3c8eb0619
Pavel <betadecay1993@gmail.com>
2019-02-15 13:33:27 +1100

    Introduced 3d NTS neural group

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import numpy as np
 import json
 
 # DEFINING PARAMETERS
-num_nrns = 14
+num_nrns = 15
 num_drives = 3
 
 # 0- PreI   # 1 - EarlyI  # 2 - PostI
@@ -17,7 +17,7 @@ params = json.load(file)
 b = np.array(params["b"])
 c = np.array(params["c"])
 
-labels = ["PreI","EarlyI", "PostI", "AugE", "RampI", "Relay", "NTS1", "NTS2", "KF","Motor_HN", "Motor_PN", "Motor_VN","KF_inh", "NTS_inh"]
+labels = ["PreI","EarlyI", "PostI", "AugE", "RampI", "Relay", "NTS1", "NTS2", "NTS3", "KF","Motor_HN", "Motor_PN", "Motor_VN","KF_inh", "NTS_inh"]
 stoptime = 60000
 res = model(b, c, vectorfield, stoptime)
 t = res[0]
--- params_gen.py
+++ params_gen.py
@@ -3,47 +3,47 @@ import numpy as np
 
 params = dict()
 
-num_nrns = 14
+num_nrns = 15
 num_drives = 3
 x = [0.5,1.0,3.5][1] # Disinh-inh of NTS
 y = [0.4,1.0,3.5][1] # Disinh-inh of KF
 
 # 0- PreI   # 1 - EarlyI  # 2 - PostI
 # 3 - AugE  # 4 - RampI   # 5 - Relay
-# 6 - NTS1  # 7 - NTS2    # 8 - KF
-# 9 - M_HN  # 10- M_PN    # 11 - M_VN
-# 12 - KF_inh # 13 - NTS_inh
+# 6 - NTS1  # 7 - NTS2    # 8 - NTS3
+# 9 - KF    # 10 - M_HN    # 11- M_PN
+# 12 - M_VN # 13 - KF_inh # 14 - NTS_inh
 b = np.zeros((num_nrns, num_nrns))
 # positive weights
 b[0,1] = 0.6  #PreI -> EarlyI
 b[0,4] = 0.9  #PreI -> RampI
-b[0,9] = 1.5 # PreI -> M_HN
-b[2,11] = 1.7 # PostI -> M_VN
-b[4,10] = 2.8 # RampI -> M_PN
-b[4,11] = 0.9 # RampI -> M_VN
+b[0,10] = 1.5 # PreI -> M_HN
+b[2,12] = 1.7 # PostI -> M_VN
+b[4,11] = 2.8 # RampI -> M_PN
+b[4,12] = 0.9 # RampI -> M_VN
 b[5,3] = 0.1 # Relay -> AugE
 b[5,6] = 0.2 # Relay -> NTS1
 b[5,7] = 1.2 # Relay -> NTS2
-b[5,8] = 2.5 # Relay -> KF
-b[5,12] = 1.3 # Relay -> KF_inh
+b[5,9] = 2.5 # Relay -> KF
+b[5,13] = 1.3 # Relay -> KF_inh
 # b[6,0] = 0.3 # NTS1 -> PreI
 # b[6,1] = 0.4 # NTS1 -> EarlyI1
 b[6,2] = 1.0 # NTS1 -> PostI
 b[6,3] = 0.09 # NTS1 -> AugE
 b[7,2] = 1.0 # NTS2 -> PostI
 b[7,3] = 0.09 # NTS2 -> AugE
-# b[7,9] = 0.9 # NTS2 -> M_HN
-# b[7,11] = 1.8 # NTS2 - M_VN
-# b[8,2] = 0.05 # KF -> PostI
-# b[8,3] = 0.01 # KF -> AugE
-b[8,6]  =  0.7 # KF -> NTS1
-b[8,7]  =  0.7 # KF -> NTS2
+# b[7,10] = 0.9 # NTS2 -> M_HN
+# b[7,12] = 1.8 # NTS2 - M_VN
+# b[9,2] = 0.05 # KF -> PostI
+# b[9,3] = 0.01 # KF -> AugE
+b[9,6]  =  0.7 # KF -> NTS1
+b[9,7]  =  0.7 # KF -> NTS2
 
 # negative weights
 b[1,2] = -0.15 #EarlyI -> PostI
 b[1,3] = -0.39 #EarlyI -> AugE
 b[1,4] = -0.15  #EarlyI1 -> RampI
-b[1,8] = -0.02 #EarlyI -> KF #!!!
+b[1,9] = -0.02 #EarlyI -> KF #!!!
 b[2,0] = -0.13 #PostI -> PreI
 b[2,1] = -0.1 #PostI -> EarlyI
 b[2,3] = -0.4 #PostI -> AugE
@@ -54,13 +54,13 @@ b[3,1] = -0.3  #AugE -> EarlyI
 b[3,2] = -0.45  #AugE -> PostI
 b[6,7] = -0.15*x #NTS1 -> NTS2
 b[7,6] = -0.15*x #NTS2 -> NTS1
-b[12,8] = -0.2*y #KF_inh -> KF
-b[13,6] = -0.2*x #NTS_inh -> NTS1
-b[13,7] = -0.2*x #NTS_inh -> NTS1
+b[13,9] = -0.2*y #KF_inh -> KF
+b[14,6] = -0.2*x #NTS_inh -> NTS1
+b[14,7] = -0.2*x #NTS_inh -> NTS1
 # b[6,0] = -0.1 #NTS1 -> PreI
 # b[7,0] = -0.2 #NTS2 -> PreI
 # b[7,4] = 0.3 #NTS1 -> RampI
-# b[8,4] = 0.2 #NTS2 -> RampI
+# b[9,4] = 0.2 #NTS2 -> RampI
 
 c = np.zeros((num_drives, num_nrns))
 # other
@@ -71,9 +71,9 @@ c[0,3] = 0.0 #To AugE
 c[0,4] = 0.0 #To RampI
 c[0,6] = 0.2 #To NTS1
 c[0,7] = 0.0 #To NTS2
-c[0,8] = 1.1 #To KF
-c[0,12] = 0.4 #To KF_inh
-c[0,13] = 0.4 #To NTS_inh
+c[0,9] = 1.1 #To KF
+c[0,13] = 0.4 #To KF_inh
+c[0,14] = 0.4 #To NTS_inh
 #Rtn/BotC
 c[1,0] = 0.0 #To PreI
 c[1,1] = 0.0 #To EarlyI
@@ -85,11 +85,7 @@ c[2,0] = 0.025 #To PreI
 b = b.tolist()
 c = c.tolist()
 
-params["description"] = \
-    "# 0- PreI   # 1 - EarlyI  # 2 - PostI \
-    # 3 - AugE  # 4 - RampI \
-    # 5 - Relay # 6 - NTS1    # 7 - NTS2\
-    # 8 - KF"
+params["description"] = ""
 
 params["b"] = b
 
