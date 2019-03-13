baf907863e77f7f7905f2e74e5fe50bd36f46ab2
Pavel <betadecay1993@gmail.com>
2019-03-13 17:22:24 +1100

    just structured the code

--- src/Picture_generator.py
+++ src/Picture_generator.py
@@ -7,19 +7,22 @@ import json
 num_nrns = 15
 num_drives = 3
 
-# 0- PreI   # 1 - EarlyI  # 2 - PostI
-# 3 - AugE  # 4 - RampI   # 5 - Relay
-# 6 - NTS1  # 7 - NTS2    # 8 - KF
-# 9 - M_HN  # 10- M_PN    # 11 - M_VN
-
 file = open("rCPG_swCPG.json", "rb+")
 params = json.load(file)
 b = np.array(params["b"])
 c = np.array(params["c"])
 
+
 labels = ["PreI","EarlyI", "PostI", "AugE", "RampI", "Relay", "NTS1", "NTS2", "NTS3", "KF","Motor_HN", "Motor_PN", "Motor_VN","KF_inh", "NTS_inh"]
 stoptime = 60000
-res = model(b, c, vectorfield, stoptime)
-t = res[0]
-signals = res[1:]
-plot_signals(t,signals[:-2], labels, stoptime)
+
+t_start = [25000 + 250*i for i in range(5)]
+t_stop = [35000 + 250*i for i in range(5)]
+for i in range(len(t_start)):
+    t1 = t_start[i]
+    t2 = t_stop[i]
+    file_name = "Normal_swallowing_" +str(t1) +"_"+ str(t2)
+    res = model(b, c, vectorfield, t1, t2, stoptime)
+    t = res[0]
+    signals = res[1:]
+    plot_signals(t,signals[:-2], labels, stoptime, file_name)
