c7aaa93b1ea880f650e3449ea90e5afd360d21b3
Pavel <betadecay1993@gmail.com>
2020-02-06 16:37:15 +1100

    Introduced inhibition fro AugE to Sw neurons. Need to fix spontaneous swallowing

--- src/params_gen.py
+++ src/params_gen.py
@@ -10,7 +10,7 @@ y = [0.1, 1.0, 10][2] # Disinh-inh of KF
 
 # 0- PreI   # 1 - EarlyI  # 2 - PostI
 # 3 - AugE  # 4 - RampI   # 5 - Relay
-# 6 - NTS1  # 7 - NTS2    # 8 - NTS3
+# 6 - Sw 1  # 7 - Sw2     # 8 - Sw3
 # 9 - KFi   # 10 - KFe    # 11 - M_HN
 # 12- M_PN  # 13 - M_VN   # 14 - KF_inh
 # 15 - NTS_inh
@@ -44,8 +44,8 @@ b[10,13] = 0.5 # KFe -> M_VN
 # negative weights
 b[1,2] = -0.3   #EarlyI -> PostI
 b[1,3] = -0.4  #EarlyI -> AugE
-b[1,6] = -0.05  #EarlyI -> NTS1
-b[1,7] = -0.05  #EarlyI -> NTS2
+b[1,6] = -0.04  #EarlyI -> Sw1
+b[1,7] = -0.04  #EarlyI -> Sw2
 b[1,4] = -0.08  #EarlyI1 -> RampI
 b[1,10] = -0.3  #EarlyI1 -> KFe
 
@@ -60,8 +60,8 @@ b[3,4] = -0.25  #AugE -> RampI
 b[3,0] = -0.56   #AugE -> PreI
 b[3,1] = -0.46  #AugE -> EarlyI
 b[3,2] = -0.04  #AugE -> PostI
-b[3,6] = -0.0   #AugE -> NTS1
-b[3,7] = -0.0   #AugE -> NTS2
+b[3,6] = -0.01 #AugE -> Sw1
+b[3,7] = -0.01 #AugE -> Sw2
 b[3,9] = -0.01  #AugE -> KFi
 b[3,10] = -0.01 #AugE -> KFe
 b[3,13] = -0.1  #AugE -> M_VN
@@ -69,21 +69,21 @@ b[3,13] = -0.1  #AugE -> M_VN
 b[5,0] = -0.2 # Relay -> PreI
 b[5,1] = -0.2 # Relay -> EarlyI
 
-b[6,0] = -0.4 #NTS1 -> PreI
-b[6,7] = -0.3*x #NTS1 -> NTS2
-b[7,6] = -0.33*x #NTS2 -> NTS1
+b[6,0] = -0.4 #Sw1 -> PreI
+b[6,7] = -0.3*x #Sw1 -> Sw2
+b[7,6] = -0.33*x #Sw2 -> Sw1
 
 b[9,0] = -0.04 #KFi -> PreI
 b[9,1] = -0.04 #KFi -> EarlyI
-b[9,6] = -0.025 #KFi -> NTS1
-b[9,7] = -0.018 #KFi -> NTS2
+b[9,6] = -0.025 #KFi -> Sw1
+b[9,7] = -0.018 #KFi -> Sw2
 
 b[14,9] = -0.3*y #KF_inh -> KFi
 b[14,10] = -0.3*y #KF_inh -> KFe
 b[15,5] = -0.3*x #NTS_inh -> Relay
-b[15,6] = -0.2*x #NTS_inh -> NTS1
-b[15,7] = -0.2*x #NTS_inh -> NTS2
-b[15,8] = -0.2*x #NTS_inh -> NTS2
+b[15,6] = -0.2*x #NTS_inh -> Sw1
+b[15,7] = -0.2*x #NTS_inh -> Sw2
+b[15,8] = -0.2*x #NTS_inh -> Sw3
 
 c = np.zeros((num_drives, num_nrns))
 # other
@@ -92,9 +92,9 @@ c[0,1] = 0.34+0.03  #To EarlyI
 c[0,2] = 0.03  #To PostI
 c[0,3] = 0.39  #To AugE
 c[0,4] = 0.3  #To RampI
-c[0,6] = 0.58 #To NTS1
-c[0,7] = 0.7  #To NTS2
-c[0,8] = 0.8  #To NTS3
+c[0,6] = 0.58 #To Sw1
+c[0,7] = 0.7  #To Sw2
+c[0,8] = 0.8  #To Sw3
 c[0,9] = 0.8  #To KFi
 c[0,10] = 0.8  #To KFe
 c[0,14] = 0.3 #To KF_inh
