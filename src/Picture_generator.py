47ef672ae5a8db260e72bf3e902de5540be5340a
Pavel <betadecay1993@gmail.com>
2020-02-13 16:40:53 +1100

    Fix bug in long stimulation

--- src/Picture_generator.py
+++ src/Picture_generator.py
@@ -1,8 +1,8 @@
 import pickle
 import numpy as np
 from matplotlib import pyplot as plt
-# plt.rcParams['text.usetex'] = True
-# plt.rcParams['text.latex.unicode'] = True
+from matplotlib import rc
+# rc('text', usetex=True)
 
 def nice_error_bar(x,y,error, title, xlabel,ylabel, save_to = None):
     fig = plt.figure(figsize = (40,20))
@@ -19,34 +19,64 @@ def nice_error_bar(x,y,error, title, xlabel,ylabel, save_to = None):
         fig.savefig("../img/" + save_to)
 
 
-info_amp = pickle.load(open("features_var_amp_5.pkl",'rb+'))
+def nice_error_bar_scatter(x,y,error, title, xlabel,ylabel, save_to = None):
+
+    fig = plt.figure(figsize = (40,20))
+    plt.errorbar(x, y, yerr = error , color = 'red', ls='', marker='o',
+             ecolor = 'gray', capsize = 3, linewidth = 3, alpha = 0.8)
+
+    plt.title(title, fontsize = 20)
+    plt.xlabel(xlabel, fontsize = 15)
+    plt.ylabel(ylabel, fontsize = 15)
+    plt.ylim(min(y-error)-0.2*abs(min(y-error)),1.2*max(y+error))
+    plt.grid(True)
+    plt.show()
+    if save_to is not None:
+        fig.savefig("../img/" + save_to)
+
+#
+info_amp = pickle.load(open("../data/features_var_amp_15_02_2020.pkl",'rb+'))
 amps = info_amp['amps']
+
 periods = info_amp['periods']
-period_stds = info_amp['period_stds']
-rough_periods = info_amp['rough_periods']
-num_swallows = info_amp['num_swallows_s']
-num_breakthroughs_AugE = info_amp['num_breakthroughs_AugE_s']
-num_breakthroughs_PreI = info_amp['num_breakthroughs_PreI_s']
+periods[periods == np.inf] = 10000
+periods = np.mean(periods, axis=1)
+
+period_stds = np.mean(info_amp['period_stds'], 1)
+period_stds[period_stds == np.inf] = 10000
+period_stds = period_stds
+
+rough_periods = np.mean(info_amp['rough_periods'], axis=1)
+rough_period_stds = np.std(info_amp['rough_periods'], axis=1)
+
+num_swallows = np.mean(info_amp['num_swallows_s'], axis=1)
+num_swallow_stds = np.std(info_amp['num_swallows_s'], axis=1)
+
+num_breakthroughs_AugE = np.mean(info_amp['num_breakthroughs_AugE_s'], axis=1)
+num_breakthroughs_AugE_stds = np.std(info_amp['num_breakthroughs_AugE_s'], axis=1)
+
+num_breakthroughs_PreI = np.mean(info_amp['num_breakthroughs_PreI_s'], axis=1)
+num_breakthroughs_PreI_stds = np.std(info_amp['num_breakthroughs_PreI_s'], axis=1)
 
 # PLOTTING PERIOD AND STD
 title = "Swallowing period and standard deviation"
 xlabel = "Amplitude of the impulse"
 ylabel = "Period of the swallowing"
-start_idx = 67
+start_idx = 0
 x = amps[start_idx:]
-y = np.mean(periods, axis = 1)[start_idx:]
-error = np.mean(period_stds, axis = 1)[start_idx:]
+y = periods[start_idx:]
+error = period_stds[start_idx:]
 nice_error_bar(x,y,error,title,xlabel,ylabel, title)
 
-# PLOTTING ROUGH_PERIODS
-title = "Swallowing period (roughly) and standard deviation"
-xlabel = "Amplitude of the impulse"
-ylabel = "Period of the swallowing"
-start_idx = 67
-x = amps[start_idx:]
-y = np.mean(rough_periods, axis = 1)[start_idx:]
-error = np.std(rough_periods, axis = 1)[start_idx:]
-nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# # PLOTTING ROUGH_PERIODS
+# title = "Swallowing period (roughly) and standard deviation"
+# xlabel = "Amplitude of the impulse"
+# ylabel = "Period of the swallowing"
+# start_idx = 0
+# x = amps[start_idx:]
+# y = rough_periods[start_idx:]
+# error = rough_period_stds[start_idx:]
+# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
 
 #PLOTTNG NUMBER OF SWALLOWS
 title = "Dependence of number of swallows on amplitude"
@@ -54,8 +84,8 @@ xlabel = "Amplitude of the impulse"
 ylabel = "Number of swallows"
 start_idx = 0
 x = amps[start_idx:]
-y = np.mean(num_swallows, axis = 1)[start_idx:]
-error = np.std(num_swallows, axis = 1)[start_idx:]
+y = num_swallows[start_idx:]
+error = num_swallow_stds[start_idx:]
 nice_error_bar(x,y,error,title,xlabel,ylabel, title)
 
 # PLOTTNG NUMBER OF Breakthroughs
@@ -64,86 +94,107 @@ xlabel = "Amplitude of the impulse"
 ylabel = "Number of breakthroughs"
 start_idx = 0
 x = amps[start_idx:]
-y = np.mean(num_breakthroughs_PreI, axis = 1)[start_idx:]
-error = np.std(num_breakthroughs_PreI, axis = 1)[start_idx:]
+y = num_breakthroughs_PreI[start_idx:]
+error = num_breakthroughs_PreI_stds[start_idx:]
 nice_error_bar(x,y,error,title,xlabel,ylabel, title)
 
-# info_phase = pickle.load(open("features_var_phase_3.pkl",'rb+'))
+
+# PLOTTNG NUMBER OF Breakthroughs
+title = "Dependence of number of AugE breakthroughs on amplitude"
+xlabel = "Amplitude of the impulse"
+ylabel = "Number of breakthroughs"
+start_idx = 0
+x = amps[start_idx:]
+y = num_breakthroughs_AugE[start_idx:]
+error = num_breakthroughs_AugE_stds[start_idx:]
+nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+
+# info_phase = pickle.load(open("../data/features_var_phase_12022020.pkl",'rb+'))
 #
 # shifts = info_phase['shift']
-# Ti_0s = info_phase['Ti_0s']
-# T0s = info_phase['T0s']
-# T1s = info_phase['T1s']
-# Phis = ((info_phase['Phis']/T0s) % 1.0)*2*np.pi
-# Thetas = info_phase['Thetas']
-# Ti_1s = info_phase['Ti_1s']
-# Ti_2s = info_phase['Ti_2s']
+# Ti_0s = np.roll(info_phase['Ti_0s'], -3, axis=0)
+# T0s = np.roll(info_phase['T0s'], -3, axis=0)
+# T1s = np.roll(info_phase['T1s'], -3, axis=0)
+# Phis = ((info_phase['Phis']/T0s) % 1.0) * 2*np.pi
+# Thetas = np.roll(info_phase['Thetas'], -3, axis=0)
+# Ti_1s = np.roll(info_phase['Ti_1s'], -3, axis=0)
+# Ti_2s = np.roll(info_phase['Ti_2s'], -3, axis=0)
 
 # #PLOTTNG Ti_0 from shifts
 # title = "Dependence of $Ti_0$ on shift"
-# xlabel = "Shift (in ms)"
+# xlabel = "Shift, rad"
 # ylabel = "$T_{i0}$"
-# start_idx = 0
+# start_idx = 3
 # x = np.mean(Phis, axis = 1)[start_idx:]
 # y = np.mean(Ti_0s, axis = 1)[start_idx:]
 # error = np.std(Ti_0s, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
 #
-# #PLOTTNG Ti_0 from shifts
+#PLOTTNG Ti_0 from shifts
 # title = "Dependence of $T_0$ on shift"
-# xlabel = "Shift (in ms)"
+# xlabel = "Shift, rad"
 # ylabel = "$T_0$"
 # start_idx = 0
 # x = np.mean(Phis, axis = 1)[start_idx:]
 # y = np.mean(T0s, axis = 1)[start_idx:]
 # error = np.std(T0s, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
 #
 # #PLOTTNG T1 from shifts
 # title = "Dependence of $T_1$ on shift"
-# xlabel = "Shift (in ms)"
+# xlabel = "Shift, rad"
 # ylabel = "$T_{1}$"
 # start_idx = 0
 # x = np.mean(Phis, axis = 1)[start_idx:]
 # y = np.mean(T1s, axis = 1)[start_idx:]
 # error = np.std(T1s, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
 #
 # # PLOTTNG Phis from shifts
 # title = "Dependence of $\Phi$ on shift"
-# xlabel = "Shift (in ms)"
+# xlabel = "Shift, rad"
 # ylabel = "$\Phi$"
 # start_idx = 0
 # x = np.mean(Phis, axis = 1)[start_idx:]
 # y = np.mean(Phis, axis = 1)[start_idx:]
 # error = np.std(Phis, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
 #
 # # PLOTTNG Thetas from shifts
 # title = "Dependence of $\Theta$ on shift"
-# xlabel = "Shift (in ms)"
+# xlabel = "Shift, rad"
 # ylabel = "$\Theta$"
 # start_idx = 0
 # x = np.mean(Phis, axis = 1)[start_idx:]
 # y = np.mean(Thetas, axis = 1)[start_idx:]
 # error = np.std(Thetas, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
-
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
+#
 # # PLOTTNG Thetas from shifts
-# title = "Dependence of $\Delta \Theta$ on \phi"
+# title = "Dependence of $\Delta \Theta$ on \Phi"
 # xlabel = "Phis (in ms)"
 # ylabel = "$\Delta \Theta"
 # start_idx = 0
-# x = np.mean(Phis, axis = 1)[start_idx:]
+# x = np.roll(np.mean(Phis, axis = 1),-2, axis=0)[start_idx:]
 # y = np.mean((T1s - T0s)/T0s, axis = 1)[start_idx:]
 # error = np.std((T1s - T0s)/T0s, axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
-
-# title = "Dependence of $(Ti_0s/T0s) on \phi"
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
+#
+# title = "Dependence of $(Ti_0s T0s reatio) on \Phi"
 # xlabel = "Phis (in ms)"
 # ylabel = "$\Delta \Theta"
 # start_idx = 0
-# x = np.mean(Phis, axis = 1)[start_idx:]
+# x = np.roll(np.mean(Phis, axis = 1), -2, axis=0)[start_idx:]
 # y = np.mean((Ti_0s/T0s), axis = 1)[start_idx:]
 # error = np.std((Ti_0s/T0s), axis = 1)[start_idx:]
-# nice_error_bar(x,y,error,title,xlabel,ylabel, title)
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
+#
+#
+# title = "PRC"
+# xlabel = "r${\Phi}, rad"
+# ylabel = "r$({T_1} - {T_0}) / {T_0}"
+# start_idx = 0
+# x = np.roll(np.mean(Phis, axis = 1), -2, axis=0)[start_idx:]
+# y = np.mean((T1s - T0s) / T0s, axis = 1)[start_idx:]
+# error = np.std(((T1s - T0s) / T0s), axis = 1)[start_idx:]
+# nice_error_bar_scatter(x,y,error,title,xlabel,ylabel, title)
--- src/features_long_impulse.py
+++ src/features_long_impulse.py
@@ -3,31 +3,34 @@ from utils import *
 from params_gen import *
 import pickle
 import json
+from tqdm.auto import tqdm
 
 if __name__ == '__main__':
-    file = open("rCPG_swCPG.json", "rb+")
+    file = open("../data/rCPG_swCPG.json", "rb+")
     params = json.load(file)
     b = np.array(params["b"])
     c = np.array(params["c"])
-    t1_s = [10000, 14000, 18000, 22000, 25000, 29000, 33000, 37000, 40000, 44000, 48000]
-    amps = [100 + i*3 for i in range(101)][::-1]
+    t1_s = [22000, 25000, 29000, 33000, 37000]
+    amps = [100 + i*3 for i in range(151)][::-1]
+    # amps = [150, 200, 300, 500]
     periods = np.empty((len(amps), len(t1_s)), dtype = float)
     period_stds = np.empty((len(amps), len(t1_s)), dtype = float)
     rough_periods = np.empty((len(amps), len(t1_s)), dtype = float)
     num_swallows_s = np.empty((len(amps), len(t1_s)), dtype = int)
     num_breakthroughs_PreI_s = np.empty((len(amps), len(t1_s)), dtype = int)
     num_breakthroughs_AugE_s = np.empty((len(amps), len(t1_s)), dtype = int)
-    for i in range(len(amps)):
+    for i in tqdm(range(len(amps))):
         for j in range(len(t1_s)):
             amp = amps[i]
             t1 = t1_s[j]
-            print("Amp: {}, time : {}".format(amp, t1))
+            # print("Amp: {}, time : {}".format(amp, t1))
             t2 = t1 + 10000
-            stoptime = 60000
+            stoptime = 50000
             signals, t = run_model(t1, t2, amp, stoptime, '10_sec_stim_diff_amp')
+            dt = 0.75
             # signals, t = pickle.load(open("../data/signals_intact_model.pkl", "rb+"))
             period, period_std, rough_period, num_swallows, num_breakthroughs_PreI, num_breakthroughs_AugE = \
-                get_features_long_impulse(signals, t, t1, t2)
+                get_features_long_impulse(signals, dt, t1, t2)
             periods[i,j] = period
             period_stds[i,j] = period_std
             rough_periods[i,j] = rough_period
@@ -45,6 +48,8 @@ if __name__ == '__main__':
     info['num_breakthroughs_AugE_s'] = num_breakthroughs_AugE_s
     info['num_breakthroughs_PreI_s'] = num_breakthroughs_PreI_s
 
+    pickle.dump(info, open('../data/features_var_amp_16_02_2020.pkl', 'wb+'))
+
     periods_avg = np.nanmean(periods, axis = 1)
     period_std_avg = np.nanmean(period_stds, axis=1)
     rough_periods_avg = np.nanmean(rough_periods, axis=1)
@@ -52,7 +57,6 @@ if __name__ == '__main__':
     num_breakthroughs_AugE_s_avg = np.nanmean(num_breakthroughs_AugE_s, axis=1)
     num_breakthroughs_PreI_s_avg = np.nanmean(num_breakthroughs_PreI_s, axis=1)
 
-    pickle.dump(info, open('features_var_amp.pkl', 'wb+'))
     nice_plot(periods_avg)
     nice_plot(period_std_avg)
     nice_plot(rough_periods_avg)
