ae183a19345e27aad3f70439eab5b4aef36b5310
Pavel <betadecay1993@gmail.com>
2020-04-30 11:00:30 +1000

    revert back to previous params

--- src/num_experiments/params_gen.py
+++ src/num_experiments/params_gen.py
@@ -15,96 +15,80 @@ def generate_params(inh_NTS, inh_KF):
     # 12 - M_HN  # 13- M_PN  # 14 - M_VN
     # 15 - KF_inh # 16 - NTS_inh # 17 - SI
     b = np.zeros((num_nrns, num_nrns))
-    # positive weights
+    # # positive weights
     b[0,1] = 0.3  #PreI -> EarlyI # Rubins (2009): (0.4)    Rubins (2011): (0.35)
     b[0,4] = 0.6  #PreI -> RampI
     b[0,12] = 0.4 # PreI -> M_HN
-
-    b[2,14] = 1.0 # PostI -> M_VN
-
+    b[2,14] = 0.25 # PostI -> M_VN
     b[4,13] = 0.6 # RampI -> M_HN
     b[4,13] = 0.5 # RampI -> M_PN
     b[4,14] = 0.6 # RampI -> M_VN
-
-    b[5,6] = 1.2 # Relay -> Sw1
-    b[5, 7] = 0.4  # Relay -> Sw2
+    b[5,2] = 0.4 # Relay -> PostI
+    b[5,6] = 0.84 # Relay -> Sw1
+    b[5,7] = 0.77 # Relay -> Sw2
     b[5,8] = 0.65 # Relay -> Sw3
-    b[5,9] = 0.35 # Relay -> KF_t
-    b[5,10] = 0.35 # Relay -> KF_p
-
-    b[6, 6] = 0.6 # Sw1 -> Sw1
-    b[6,12] = 0.6 # Sw1 -> M_HN
+    b[5,9] = 0.4 # Relay -> KF_t
+    b[5,10] = 0.4 # Relay -> KF_p
+    b[6,12] = 0.5 # Sw1 -> M_HN
     b[6,14] = 0.6 # Sw1 -> M_VN
-
     b[8,1] = 0.2 # Sw3 -> EarlyI
-    b[8,2] = 0.35 # Sw3 -> PostI
-    b[8, 7] = 0.3  # Sw3 -> Sw2
-
-    b[9, 11] = 1.4  # KF_t -> KF_relay
-
-    b[10,2] = 0.45 # KF_p -> PostI
+    b[8,2] = 0.4 # Sw3 -> PostI
+    b[10,2] = 0.85 # KF_p -> PostI
     b[10,8] = 0.5 # KF_p -> Sw3
-
+    b[10,14] = 0.38 # KF_p -> M_VN
+    b[9, 11] = 1.4  # KF_t -> KF_relay
     b[17, 5] = 0.6  # SI -> Relay
 
     # negative weights
+    # negative weights
     b[1, 0] = -0.02  # EarlyI -> PreI #in Rubins: (0)    Rubins (2011): (0)
-    b[1,2] = -0.33   #EarlyI -> PostI #in Rubins: (0.25)    Rubins (2011): (0.2)
-    b[1,3] = -0.4  #EarlyI -> AugE #in Rubins: (0.35)    Rubins (2011): (0.25)
+    b[1, 2] = -0.3  # EarlyI -> PostI #in Rubins: (0.25)    Rubins (2011): (0.2)
+    b[1, 3] = -0.4  # EarlyI -> AugE #in Rubins: (0.35)    Rubins (2011): (0.25)
     # b[1,6] = -0.05  #EarlyI -> Sw1
-    b[1,4] = -0.15  #EarlyI1 -> RampI
-    b[1,10] = -0.3  #EarlyI1 -> KF_p
+    b[1, 4] = -0.15  # EarlyI1 -> RampI
+    b[1, 10] = -0.3  # EarlyI1 -> KF_p
 
-    b[2,0] = -0.25    #PostI -> PreI #in Rubins: (0.3)    Rubins (2011): (0.8)
-    b[2,1] = -0.45  #PostI -> EarlyI #in Rubins: (0.05)    Rubins (2011): (0.15)
-    b[2,3] = -0.35   #PostI -> AugE #in Rubins: (0.35)    Rubins (2011): (0.4)
-    b[2,4] = -0.67   #PostI -> RampI
-    b[2,6] = -0.07  #PostI -> Sw1
-    b[2,7] = -0.07  #PostI -> Sw2
+    b[2, 0] = -0.16  # PostI -> PreI #in Rubins: (0.3)    Rubins (2011): (0.8)
+    b[2, 1] = -0.35  # PostI -> EarlyI #in Rubins: (0.05)    Rubins (2011): (0.15)
+    b[2, 3] = -0.35  # PostI -> AugE #in Rubins: (0.35)    Rubins (2011): (0.4)
+    b[2, 4] = -0.67  # PostI -> RampI
+    b[2, 6] = -0.06  # PostI -> Sw1
+    b[2, 7] = -0.07  # PostI -> Sw2
 
-    b[3,0] = -0.60  #AugE -> PreI #in Rubins: (0.2)    Rubins (2011): (0.22)
-    b[3,1] = -0.50  #AugE -> EarlyI #in Rubins: (0.35)    Rubins (2011): (0.08)
-    b[3,2] = -0.03  #AugE -> PostI #in Rubins: (0.1)    Rubins (2011): (0.0)
-    b[3,4] = -0.67  #AugE -> RampI
-    b[3,6] = -0.02  #AugE -> Sw1
-    b[3,7] = -0.02  #AugE -> Sw2
+    b[3, 0] = -0.55  # 0.55 AugE -> PreI #in Rubins: (0.2)    Rubins (2011): (0.22)
+    b[3, 1] = -0.44  # AugE -> EarlyI #in Rubins: (0.35)    Rubins (2011): (0.08)
+    b[3, 2] = -0.04  # AugE -> PostI #in Rubins: (0.1)    Rubins (2011): (0.0)
+    b[3, 4] = -0.67  # AugE -> RampI
+    b[3, 6] = -0.01  # AugE -> Sw1
+    b[3, 7] = -0.02  # AugE -> Sw2
 
-    b[5,0] = -0.20  #Relay -> PreI
-    b[5,1] = -0.20  # Relay -> EarlyI
+    b[5, 0] = -0.2  # Relay -> PreI
+    b[5, 1] = -0.2  # Relay -> EarlyI
 
+    b[6, 7] = -0.3 * x  # Sw1 -> Sw2
+    b[7, 6] = -0.35 * x  # Sw2 -> Sw1
 
-    b[6,0] = -0.1 #Sw1 -> PreI
-    b[6,1] = -0.1 #Sw1 -> EarlyI
+    b[11, 0] = -0.07  # KF_relay -> PreI
+    b[11, 1] = -0.06  # KF_relay -> EarlyI
+    b[11, 6] = -0.08  # KF_relay -> Sw1
+    b[11, 7] = -0.08  # KF_relay -> Sw2
 
-    b[7, 0] = -0.075 # Sw2 -> PreI
-    b[7, 1] = -0.075 # Sw2 -> EarlyI
-    b[6,7] = -0.34*x #Sw1 -> Sw2
-    # b[6, 8] = -0.4*x  # Sw1 -> Sw3
-    b[7,6] = -0.25*x #Sw2 -> Sw1
-
-    b[11,0] = -0.07 #KF_relay -> PreI
-    b[11,1] = -0.06 #KF_relay -> EarlyI
-    b[11,6] = -0.09 #KF_relay -> Sw1
-    b[11,7] = -0.08 #KF_relay -> Sw2
-
-    b[15,9] = -0.3*y #KF_inh -> KF_t
-    b[15,10] = -0.3*y #KF_inh -> KF_p
-
-    b[16,5] = -0.3*x #NTS_inh -> Relay
-    b[16,6] = -0.2*x #NTS_inh -> Sw1
-    b[16,7] = -0.2*x #NTS_inh -> Sw2
-    b[16,8] = -0.2*x #NTS_inh -> Sw3
-
+    b[15, 9] = -0.3 * y  # KF_inh -> KF_t
+    b[15, 10] = -0.3 * y  # KF_inh -> KF_p
+    b[16, 5] = -0.3 * x  # NTS_inh -> Relay
+    b[16, 6] = -0.2 * x  # NTS_inh -> Sw1
+    b[16, 7] = -0.2 * x  # NTS_inh -> Sw2
+    b[16, 8] = -0.2 * x  # NTS_inh -> Sw3
 
     c = np.zeros((num_drives, num_nrns))
     # other
-    c[0,0] = 0.35 #To PreI
-    c[0,1] = 0.45  #To EarlyI
-    c[0,2] = 0.04  #To PostI
+    c[0,0] = 0.262 #To PreI
+    c[0,1] = 0.38  #To EarlyI
+    c[0,2] = 0.032  #To PostI
     c[0,3] = 0.385  #To AugE
     c[0,4] = 0.53  #To RampI
-    c[0,6] = 0.4 #To Sw1
-    c[0,7] = 0.6  #To Sw2
+    c[0,6] = 0.63 #To Sw1
+    c[0,7] = 0.74  #To Sw2
     c[0,8] = 0.8  #To Sw3
     c[0,9] = 0.8  #To KF_t
     c[0,10] = 0.8  #To KF_p
