6aecdbe768e454897c4d1946cc60e2b73cc51951
Pavel <betadecay1993@gmail.com>
2020-05-02 12:57:47 +1000

    Acceptable model

--- src/num_experiments/params_gen.py
+++ src/num_experiments/params_gen.py
@@ -21,8 +21,8 @@ def generate_params(inh_NTS, inh_KF):
     b[0,12] = 0.4 # PreI -> M_HN
     b[2,14] = 0.25 # PostI -> M_VN
     b[4,13] = 0.6 # RampI -> M_HN
-    b[4,13] = 0.5 # RampI -> M_PN
-    b[4,14] = 0.6 # RampI -> M_VN
+    b[4,13] = 0.85 # RampI -> M_PN
+    b[4,14] = 0.75 # RampI -> M_VN
     b[5,2] = 0.4 # Relay -> PostI
     b[5,6] = 0.69 # Relay -> Sw1
     b[5,7] = 0.71 # Relay -> Sw2
@@ -32,8 +32,8 @@ def generate_params(inh_NTS, inh_KF):
     b[6,12] = 0.5 # Sw1 -> M_HN
     b[6,14] = 0.6 # Sw1 -> M_VN
     b[8,1] = 0.2 # Sw3 -> EarlyI
-    b[8,2] = 0.4 # Sw3 -> PostI
-    b[10,2] = 0.85 # KF_p -> PostI
+    b[8,2] = 0.7 # Sw3 -> PostI
+    b[10,2] = 0.6 # KF_p -> PostI
     b[10,8] = 0.5 # KF_p -> Sw3
     b[10,14] = 0.38 # KF_p -> M_VN
     b[9, 11] = 1.4  # KF_t -> KF_relay
@@ -45,7 +45,7 @@ def generate_params(inh_NTS, inh_KF):
     b[1, 2] = -0.3  # EarlyI -> PostI #in Rubins: (0.25)    Rubins (2011): (0.2)
     b[1, 3] = -0.4  # EarlyI -> AugE #in Rubins: (0.35)    Rubins (2011): (0.25)
     # b[1,6] = -0.05  #EarlyI -> Sw1
-    b[1, 4] = -0.15  # EarlyI1 -> RampI
+    b[1, 4] = -0.25  # EarlyI1 -> RampI
     b[1, 10] = -0.3  # EarlyI1 -> KF_p
 
     b[2, 0] = -0.16  # PostI -> PreI #in Rubins: (0.3)    Rubins (2011): (0.8)
@@ -72,8 +72,8 @@ def generate_params(inh_NTS, inh_KF):
 
     b[11, 0] = -0.05  # KF_relay -> PreI
     b[11, 1] = -0.05  # KF_relay -> EarlyI
-    # b[11, 6] = -0.08  # KF_relay -> Sw1
-    # b[11, 7] = -0.08  # KF_relay -> Sw2
+    b[11, 6] = -0.05  # KF_relay -> Sw1
+    b[11, 7] = -0.05  # KF_relay -> Sw2
 
     b[15, 9] = -0.3 * y  # KF_inh -> KF_t
     b[15, 10] = -0.3 * y  # KF_inh -> KF_p
@@ -86,10 +86,10 @@ def generate_params(inh_NTS, inh_KF):
     # other
     c[0,0] = 0.262 #To PreI
     c[0,1] = 0.39  #To EarlyI
-    c[0,2] = 0.032  #To PostI
+    c[0,2] = 0.02  #To PostI
     c[0,3] = 0.385  #To AugE
     c[0,4] = 0.53  #To RampI
-    c[0,6] = 0.33 #To Sw1
+    c[0,6] = 0.34 #To Sw1
     c[0,7] = 0.45  #To Sw2
     c[0,8] = 0.3  #To Sw3
     c[0,9] = 0.8  #To KF_t
