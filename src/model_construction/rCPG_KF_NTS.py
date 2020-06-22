0fcf006ce66edccdc3f825738884df6c35f63ef4
Pavel <betadecay1993@gmail.com>
2020-06-22 13:59:05 +1000

    Added nerve outputs. Complete model

--- src/model_construction/rCPG_KF_NTS.py
+++ src/model_construction/rCPG_KF_NTS.py
@@ -33,26 +33,28 @@ def set_weights_and_drives(x, y, population_names):
     p = population_names
     N = len(p)
     W = np.zeros((N,N))
-    Max_inhibition = 2
 
     W[p.index("PreI"), p.index("EarlyI")] = 0.40 # PreI -> EarlyI
     W[p.index("PreI"), p.index("PostI")] = 0.00 # PreI -> PostI
     W[p.index("PreI"), p.index("AugE")] = 0.00 # PreI -> AugE
+    W[p.index("PreI"), p.index("RampI")] = 0.90 # PreI -> RampI
 
     W[p.index("EarlyI"), p.index("PreI")] = -0.08 # EarlyI -> PreI
     W[p.index("EarlyI"), p.index("PostI")] = -0.25 # EarlyI -> PostI
     W[p.index("EarlyI"), p.index("AugE")] = -0.63 # EarlyI -> AugE
     W[p.index("EarlyI"), p.index("KF_p")] = -0.10 # EarlyI -> KF_p
     W[p.index("EarlyI"), p.index("Sw1")] = -0.003 # EarlyI -> Sw1
-    # W[p.index("EarlyI"), p.index("Sw2")] = -0.001 # EarlyI -> Sw2
+    W[p.index("EarlyI"), p.index("RampI")] = -0.15 # EarlyI -> RampI
 
     W[p.index("PostI"), p.index("PreI")] = -0.35 # PostI -> PreI
     W[p.index("PostI"), p.index("EarlyI")] = -0.22 # PostI -> EarlyI
     W[p.index("PostI"), p.index("AugE")] = -0.36 # PostI -> AugE
+    W[p.index("PostI"), p.index("RampI")] = -0.50 # PostI -> RampI
 
     W[p.index("AugE"), p.index("PreI")] = -0.30 # AugE -> PreI
     W[p.index("AugE"), p.index("EarlyI")] = -0.43 # AugE -> EarlyI
     W[p.index("AugE"), p.index("PostI")] = -0.06 # AugE -> PostI
+    W[p.index("AugE"), p.index("RampI")] = -0.50 # AugE -> RampI
 
     W[p.index("KF_t"), p.index("PreI")] = +0.16 * x # KF_t -> PreI
     W[p.index("KF_t"), p.index("EarlyI")] = +0.66 * x # KF_t -> EarlyI
@@ -68,7 +70,6 @@ def set_weights_and_drives(x, y, population_names):
     W[p.index("KF_relay"), p.index("Sw1")] = -0.09  # KF_relay -> Sw1
     W[p.index("KF_relay"), p.index("Sw2")] = -0.05  # KF_relay -> Sw2
 
-
     W[p.index("NTS_drive"), p.index("PostI")] = 0.42  # NTS_drive -> PostI
 
 
@@ -81,11 +82,14 @@ def set_weights_and_drives(x, y, population_names):
     W[p.index("Relay"), p.index("PreI")] = -0.30 * y # Relay -> PreI
     W[p.index("Relay"), p.index("EarlyI")] = -0.30 * y  # Relay -> EarlyI
     W[p.index("Relay"), p.index("AugE")] = -0.30 * y # Relay -> AugE
+    W[p.index("Relay"), p.index("RampI")] = -0.30 * y # Relay -> RampI
     W[p.index("Relay"), p.index("KF_t")] = 0.15 * x * y  # Relay -> KF_t
     W[p.index("Relay"), p.index("KF_p")] = 0.15 * x * y # Relay -> KF_p
     W[p.index("Relay"), p.index("Sw1")] = 0.74 * y  # Relay -> Sw1
     W[p.index("Relay"), p.index("Sw2")] = 0.71* y  # Relay -> Sw2
     W[p.index("Relay"), p.index("NTS_drive")] = 0.15 * y  # Relay -> NTS_drive
+
+
 
     drives = np.zeros((3, N))
     # other
@@ -100,6 +104,7 @@ def set_weights_and_drives(x, y, population_names):
     drives[1, p.index("EarlyI")] = 0.27 # -> EarlyI
     drives[1, p.index("PostI")] = 0.00 # -> PostI
     drives[1, p.index("AugE")] = 0.42 # -> AugE
+    drives[1, p.index("RampI")] = 0.50 # -> RampI
 
     #PreBotC
     drives[2, p.index("PreI")] = 0.025  # -> PreI
@@ -119,6 +124,7 @@ def construct_model(population_names, W, drives, dt, default_neural_params):
     Sw2 = NeuralPopulation("Sw2", default_neural_params)
     NTS_drive = NeuralPopulation("NTS_drive", default_neural_params)
     Relay = NeuralPopulation("Relay", default_neural_params)
+    RampI = NeuralPopulation("RampI", default_neural_params)
 
     # modifications:
     PreI.g_NaP = 5.0
@@ -126,6 +132,7 @@ def construct_model(population_names, W, drives, dt, default_neural_params):
     PreI.slope = 8
     PostI.K_ad = 1.3
     PostI.tau_ad = 5000.0
+    KF_p.tau_ad = 6000.0
     Relay.tau_ad = 15000.0
     Sw1.tau_ad = 1000.0
     Sw2.tau_ad = 1000.0
@@ -150,6 +157,45 @@ def run_model(net, start, stop, amplitude, duration):
     net.run(int((stop - (start + duration)) / dt))
     return None
 
+def plot(net):
+    start = 10000
+    V_array = np.array(net.v_history).T
+    t_array = np.array(net.t)
+    N = net.N
+    fig, axes = plt.subplots(N+3, 1, figsize=(25, 15))
+    if type(axes) != np.ndarray: axes = [axes]
+    fr = net.firing_rate(V_array.T, net.V_half, net.slope).T
+    for i in range(net.N):
+        if i == 0: axes[i].set_title('Firing Rates', fontdict={"size" : 20})
+        axes[i].plot(t_array[start:], fr[i, start:], 'k', linewidth=3, label=str(net.names[i]), alpha=0.9)
+        axes[i].legend(loc = 1, fontsize=25)
+        axes[i].set_ylim([-0.0, 1.0])
+        axes[i].set_yticks([])
+        axes[i].set_yticklabels([])
+        if i != len(axes) - 1:
+            axes[i].set_xticks([])
+            axes[i].set_xticklabels([])
+        axes[i].set_xlabel('t, ms', fontdict={"size" : 20})
+
+    PNA = 0.1 * fr[net.names.index("PreI"), start:] + 0.9 * fr[net.names.index("RampI"), start:]
+    HNA = 0.7 * fr[net.names.index("PreI"), start:] + 0.15 * fr[net.names.index("RampI"), start:] \
+          + 0.35 * fr[net.names.index("Sw1"),start:]
+    VNA = 0.75 * fr[net.names.index("RampI"), start:] + 0.8 * fr[net.names.index("Sw1"), start:] + \
+          0.6 * fr[net.names.index("PostI"), start:] + 0.4 * fr[net.names.index("KF_p"), start:]
+    Motor_outputs = [PNA, HNA, VNA]
+    motor_outputs_names = ["PNA", "HNA", "VNA"]
+    for i in range(N, N+3):
+        axes[i].plot(t_array[start:], Motor_outputs[i - N], 'k', linewidth=3, label=str(motor_outputs_names[i - N]), alpha=0.9)
+        axes[i].legend(loc=1, fontsize=20)
+        axes[i].set_ylim([-0.0, 1.0])
+        axes[i].set_yticks([])
+        axes[i].set_yticklabels([])
+        if i != len(axes) - 1:
+            axes[i].set_xticks([])
+            axes[i].set_xticklabels([])
+        axes[i].set_xlabel('t, ms', fontdict={"size": 20})
+    plt.subplots_adjust(wspace=0.01, hspace=0)
+    return fig, axes
 
 if __name__ == '__main__':
     data_folder = str(get_project_root()) + "/data"
@@ -157,7 +203,7 @@ if __name__ == '__main__':
     default_neural_params = json.load(open(f'{data_folder}/params/default_neural_params.json', 'r+'))
 
     population_names = ['PreI', 'EarlyI', "PostI", "AugE", "KF_t", "KF_p", "KF_relay", "NTS_drive",
-                        "Sw1", "Sw2", "Relay"]
+                        "Sw1", "Sw2", "Relay", "RampI"]
     dt = 0.5
     stoptime = 60000
     amp = 150
@@ -172,7 +218,8 @@ if __name__ == '__main__':
         Network_model = construct_model(population_names, W, drives, dt, default_neural_params)
         run_model(Network_model, start, stoptime, amp, stim_duration)
 
-        fig, axes = Network_model.plot()
+        fig, axes = plot(Network_model)
+        # fig, axes = Network_model.plot()
         folder_save_img_to = img_folder + "/" + f"other_plots/{str(date.today())}"
         fig.savefig(folder_save_img_to + "/" + f"rCPG_swCPG_{amp}_{stim_duration}_{postfix}" + ".png")
         plt.close(fig)
@@ -188,8 +235,8 @@ if __name__ == '__main__':
         print(amp, stim_duration, start)
         Network_model = construct_model(population_names, W, drives, dt, default_neural_params)
         run_model(Network_model, start, stoptime, amp, stim_duration)
-        fig, axes = Network_model.plot()
-
+        # fig, axes = Network_model.plot()
+        fig, axes = plot(Network_model)
         folder_save_img_to = img_folder + "/" + f"other_plots/{str(date.today())}"
         fig.savefig(folder_save_img_to + "/" + f"rCPG_swCPG_{amp}_{stim_duration}_{start}_{postfix}" + ".png")
         plt.close(fig)
